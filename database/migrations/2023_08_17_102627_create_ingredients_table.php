d030e8ac63b4d7e64bd6d18d34f5174a8135058c
yuuya210618 <yuuya210618@gmail.com>
2023-08-19 19:20:06 +0900

    タイムゾーン変更

--- database/migrations/2023_08_17_102627_create_ingredients_table.php
+++ database/migrations/2023_08_17_102627_create_ingredients_table.php
@@ -15,6 +15,7 @@ class CreateIngredientsTable extends Migration
     {
         Schema::create('ingredients', function (Blueprint $table) {
             $table->bigIncrements('id');
+            
             $table->timestamps();
         });
     }

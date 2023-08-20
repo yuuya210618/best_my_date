0d6babf9394457d4d5629db72021aa427db9fa8e
yuuya210618 <yuuya210618@gmail.com>
2023-08-20 20:19:54 +0900

    コントローラー作成

---
+++ app/Http/Controllers/IngredientController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Ingredient; 
+
+class IngredientController extends Controller
+{
+  
+    public function welcome()
+    {
+        $ingredients = Ingredient::all();
+
+        return view('welcome', compact('ingredients'));
+    }
+
+    public function store(Request $request)
+    {
+        $ingredient = new Ingredient(); 
+        $ingredient->ingredients_name = $request->ingredients_name; 
+        $ingredient->save();
+
+        return redirect()->action([IngredientController::class, 'welcome']);
+    }
+
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,17 @@
     <title>Best_my_date</title>
 </head>
 <body>
-    <form method="post" action="">
+    <form method="post" action="{{ route('store') }}">
+    @csrf
         <input type="ingredients_name" name="ingredients_name">
         <button>食品追加</button>
     </form>
+
+    <ul>
+        @foreach ($ingredients as $ingredient)
+            <li>{{ $ingredient->ingredients_name }}</li>
+        @endforeach
+    </ul>
+
 </body>
 </html>

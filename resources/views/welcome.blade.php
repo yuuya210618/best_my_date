dbd3e73033aa552ef456a9f250c40d59815b885b
yuuya210618 <yuuya210618@gmail.com>
2023-08-23 15:56:54 +0900

    残り日数表示

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,10 +13,17 @@
         @foreach ($ingredients as $ingredient)
             <li><span>食材名：{{ $ingredient->ingredients_name }}</span></li>
             <span>賞味期限：{{ $ingredient->best_my_date }}</span>
+            <?php
+            $day1 = new DateTime($ingredient->best_my_date);
+            $day2 = new DateTime('today');
+ 
+            $interval = $day1->diff($day2);
+ 
+            echo $interval->format('残り日数 %a日');
+            ?>
         @endforeach
         </ui>
     </div>
-    
 
 </body>
 </html>

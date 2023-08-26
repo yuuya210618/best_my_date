3dbfc89988c4d31df9a83ebb4ece494446cc4cd8
yuuya210618 <yuuya210618@gmail.com>
2023-08-26 19:27:42 +0900

    ブーストラップ導入

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,5 +1,8 @@
 @include('header')
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 
 <h2>食材を登録しよう！</h2>
 <form method="post" action="{{ route('store') }}" style="display: flex; flex-direction: column;">
@@ -10,7 +13,7 @@
         <label for="賞味期限">賞味期限</label>
         <input type='date' name="best_my_date" id="best_my_date" >
 
-        <button>食品追加</button>
+        <button type="button" class="btn btn-outline-primary">食品追加</button>
     </form>
 
     <a href="{{ route('welcome') }}">戻る</a>

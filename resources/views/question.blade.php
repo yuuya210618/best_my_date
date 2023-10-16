0b38e9b702d90d3a3444d80027eabf1ddd9a5029
yuuya210618 <yuuya210618@gmail.com>
2023-10-16 14:37:14 +0900

    Q&Aページレイアウト

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -1,4 +1,4 @@
-
+<div class="p-3 mb-2 bg-light text-dark">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
@@ -41,3 +41,5 @@
     </style>
 
 <button type="button" class="btn btn-warning"><a href="{{ route('welcome') }}">戻る</a></button>
+
+      </div>
